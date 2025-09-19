2f82b42923b213c3b34a51734314b0aedf2f6864
Roktim Taye <roktimtayecoding@gmail..com>
2025-09-19 22:33:56 +0530

    19 sep second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
   return (
     <section className="section hero" id="hero">
-      <h1>A HYGIENIC LIFESTYLE</h1>
+      <h1>THE QUICKEST LAUNDRY</h1>
     </section>
   );
 };
